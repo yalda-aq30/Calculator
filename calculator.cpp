2cedebd7d28ba9feb5ca59afa6462b6cbf5bc3eb
yalda-aq30 <yaldaaghasi85@gmail.com>
2025-09-20 16:52:54 +0330

    everything is ok exept multitasking and empty inputs for second and third inputs!

---
+++ calculator.cpp
@@ -0,0 +1,138 @@
+#include <iostream> 
+#include <string> 
+#include <cctype>
+#include <sstream>
+
+
+double operate(double number_1 ,char operatorValue, double number_2 ){
+    if (operatorValue == '+') {
+        return number_1 + number_2;
+    }
+    else if (operatorValue == '-') {
+        return number_1 - number_2;
+    }
+    else if (operatorValue == '*') {
+        return number_1 * number_2;
+    }
+    else if (operatorValue == '/'){
+        if (number_2 == 0) {
+            std::cout << "Error! second number should not be zero!" << std::endl; 
+            return 0;
+        }
+        return number_1 / number_2 ; 
+    } 
+    else {
+        std::cout << "Unknown operator!" << std::endl;
+        return 0;
+    }
+
+} 
+
+bool isNumber(const std::string &s){
+    bool dotSeen = false;
+    for (char c : s) {
+        if (c == '.') {
+            if (dotSeen) return false;
+            dotSeen = true; 
+        } 
+        else if (!isdigit(c)) {
+            return false;
+        }
+    }
+    return !s.empty(); 
+}
+
+
+void numericRange(double number_1 , char &opr , double number_2) { 
+    if (number_1 > 99999 ||  number_1 < -99999 || number_2 > 99999 || number_2 < -99999) {
+        std::cout << "Error! Numbers must be between -99999 and 99999. " << std::endl;
+        opr = 'e'; 
+    }
+}
+
+
+
+
+void getInput(double &number_1 , char &opr , double &number_2){
+
+    std::cout<< "If you want to quit program please enter exit! " << std::endl;
+    std::cout<< "Enter your operation: "; 
+
+    std::string line; 
+    std::getline(std::cin, line);
+    
+    if (line.empty()){
+        std::cout << "Please enter a valid operation! " << std::endl;
+        opr = 'e';
+        return;
+    }
+    
+    
+    if (line == "exit"){
+        opr = 'q';
+        return;
+    }
+
+    std::string num1_str, num2_str;
+    std::stringstream ss(line);
+
+    if (!(ss >> num1_str >> opr >> num2_str)) {
+        std::cout <<"Error! Please enter in format: number operator number" << std::endl;
+        opr = 'e';
+        return;
+    }
+
+
+    if (!isNumber(num1_str) || !isNumber(num2_str)) {
+        std::cout << "Error! Please enter correct number..." << std::endl;  
+        opr = 'e'; 
+        return;
+    } 
+
+    number_1 = std::stod(num1_str); 
+    number_2 = std::stod(num2_str); 
+    
+    
+
+
+
+
+    
+  
+    
+     
+}
+
+
+
+
+
+
+int main(){ 
+    while (true){
+        double num1 , num2; 
+        char op;
+
+        
+
+        getInput(num1,op,num2); 
+        numericRange(num1,op,num2) ; 
+        if (op == 'q') {
+            std::cout << "Exiting program..." << std::endl; 
+            break;
+        } 
+
+        if (op == 'e') {
+            continue;
+        }
+
+
+        double result = operate (num1,op,num2);
+        std::cout << "Result = " << result << std::endl; 
+
+
+        
+
+    }
+return 0; 
+}
---
+++ first.cpp
@@ -0,0 +1,93 @@
+#include <iostream> 
+#include <string> 
+
+
+int operate(int a , int b, char operatorValue){
+    // All calcuations should be here
+
+    // return the result
+}
+
+// Learn about refrences 
+int getInput(int &numberA,int &numberB, char operatorValue){
+    numberA = 2;
+    // Input Here
+}
+
+
+
+
+int main() {
+
+    while(true){
+        std::cout << "calculator: " << std::endl;
+        int a,b;
+
+        getInput(a,b,z);
+        int result = operate(x,y,z);
+        std::cout << "Result :  " << result << std::endl;
+    }
+
+
+
+
+
+    // ----- 
+    std::string input; 
+    double number_1;
+    double number_2;
+    char opr; 
+    
+    while (true) {
+        std::cout << "calculator: " << std::endl;
+        std::cin >> input;
+        if (input == "exit" || input == "EXIT" || input == "Exit") break; 
+        try { 
+            number_1 = std::stod(input); 
+        } catch (...) {
+            std::cout << "Invalid number!" << std::endl;
+            continue; // برگرد به ابتدای حلقه 
+        }
+        std::cin >> input;
+        if (input == "exit") break;
+        opr = input[0]; 
+        std::cin >> input;
+        if (input == "exit") break;
+        try {
+            number_2 = std::stod(input);
+        } catch (...) {
+            std::cout << "Invalid number!" << std::endl;
+            continue; // برگرد به اول حلقه 
+        } 
+        // std::cin >> number_1;
+        // std::cin >> opr;
+        // std::cin >> number_2;
+        if (number_1 > 999999999 || number_1 < -999999999 || number_2 > 999999999 || number_2 < -999999999){
+        std::cout << "Error: number out of allowed range!" << std::endl;
+        continue; 
+    }
+    switch (opr) { 
+        case '+':
+            std::cout << number_1 + number_2 << std::endl;
+            break;
+        case '-':
+            std::cout << number_1 - number_2 << std::endl;
+            break;
+        case '*':
+            std::cout << number_1 * number_2 << std::endl;
+            break;
+        case '/': 
+            if (number_2 == 0) {
+                std::cout << "undefined" << std::endl;
+            }
+            else {
+                std::cout << number_1/number_2 << std::endl;
+            }
+        break; 
+        default:
+            std::cout << "Unknown operator!" << std::endl;  
+    } 
+    
+    }
+    return 0; 
+}
